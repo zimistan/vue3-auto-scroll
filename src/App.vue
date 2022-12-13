5605c6b5ea360fbe9de1a1fb3fcbd5a385d0af8b
adil <a__dil@outlook.com>
2022-12-13 11:17:42 +0800

    配置滚动速度

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
+let status = ref(true)
+
+const click = () => {
+  status.value = !status.value
+}
+
 
 </script>
 
 <template>
-  <div class="scroller" v-auto-scroll>
+  <div class="scroller" v-auto-scroll="status">
     <p>1</p>
     <p>1</p>
     <p>1</p>
@@ -18,6 +26,7 @@
     <p>1</p>
     <p>1</p>
   </div>
+  <button @click="click">{{ status ? '开始' : '暂停' }}</button>
 </template>
 
 <style scoped lang="scss">
@@ -26,7 +35,6 @@
   width: 400px;
   overflow: scroll;
   background-color: #444;
-  resize:auto;
 }
 
 </style>
--- src/directive/autoScroll.ts
+++ src/directive/autoScroll.ts
@@ -1,30 +1,41 @@
 import {App, DirectiveBinding, VNode} from "vue";
 
+//这个属性得替换
+let speed = 20
 const autoScroll = (app: App) => app.directive('autoScroll', scrollDirective)
 
 const scrollDirective = {
   mounted(el: HTMLElement, binding: DirectiveBinding<any>) {
     startScroll(el)
+  },
+  updated(el: HTMLElement, binding: DirectiveBinding<any>) {
   }
 }
-
+//开始滚动
 const startScroll = (element: HTMLElement) => {
-  console.log("start:", element)
-  window.requestAnimationFrame(() => scroll(element))
+  return window.requestAnimationFrame(() => scroll(element))
 }
-
-const scroll = (element: HTMLElement, timeStamp: number = 0, oldTime: number = 0) => {
-  // console.log(element.scrollTop++);
+//滚动
+const scroll = (element: HTMLElement, oldTime: number = 0, timeStamp: number = 0) => {
   window.requestAnimationFrame((time) => {
-    if (timeStamp > 2000) {
+    if (timeStamp > 1000 / speed) {
+      element.scrollTop += calcPxSpeed()
       timeStamp = 0
-      scroll(element, timeStamp,time)
     }
+    scroll(element, time, timeStamp + time - oldTime)
   })
 }
-
+//停止滚动
 const stopScroll = (element: HTMLElement) => {
   console.log("stop:", element)
+}
+
+const calcPxSpeed = (): number => {
+  if (speed < 1000) {
+    return 1
+  } else {
+    return Number((speed / 1000).toFixed(1))
+  }
 }
 
 export {
